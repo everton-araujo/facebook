5a34a9b8bbecde7b7cd8c16ee9cb1ff11e015fa7
Everton <everton.aaraujo@outlook.com>
2022-04-04 21:11:29 -0300

    Login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { signOut, useSession } from 'next-auth/react';
 import {
   BellIcon,
   ChatIcon,
@@ -17,6 +18,8 @@ import {
 import { HeaderIcon } from "./HeaderIcon";
 
 export function Header() {
+  const {data: session} = useSession();
+
   return (
     <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* Left */}
@@ -33,7 +36,7 @@ export function Header() {
           <input
             type="text"
             placeholder="Search Facebook"
-            className="hidden md:inline-flex flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
           />
         </div>
       </div>
@@ -51,7 +54,16 @@ export function Header() {
 
       {/* Right */}
       <div className="flex items-center sm:space-x-2 justify-end">
-        <p className="whitespace-nowrap font-semibold pr-3">Everton</p>
+        <Image
+          onClick={() => signOut()}
+          src={session.user.image}
+          width={40}
+          height={40}
+          layout='fixed'
+          className='rounded-full cursor-pointer'
+        />
+
+        <p className="whitespace-nowrap font-semibold pr-3">{session.user.name}</p>
         <ViewGridIcon className="icon" />
         <ChatIcon className="icon" />
         <BellIcon className="icon" />
